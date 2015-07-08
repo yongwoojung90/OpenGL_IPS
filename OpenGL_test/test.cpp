ce05bb359478539a5f96fbfd35014aeac2c53248
yongwoojung90 <loyyd03@gmail.com>
2015-07-08 14:44:24 +0900

    2nd commit test

--- OpenGL_test/test.cpp
+++ OpenGL_test/test.cpp
@@ -6,6 +6,9 @@
 *		Visit My Site At nehe.gamedev.net
 */
 
+// git commit test comment
+
+
 #include <windows.h>		// Header File For Windows
 #include <gl\gl.h>			// Header File For The OpenGL32 Library
 #include <gl\glu.h>			// Header File For The GLu32 Library
